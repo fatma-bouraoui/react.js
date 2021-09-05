920ae8b201d66fd0f78555899afa7e4e1701f919
fatma-bouraoui <88318777+fatma-bouraoui@users.noreply.github.com>
2021-09-06 01:42:28 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Adresse from './components/Adresse';
+import Fullname from './components/Fullname';
+import Profilephoto from './components/Profilephoto';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Fullname/>
+     <Adresse></Adresse>
+     <Profilephoto/>
+ 
+    </div>
+  );
+}
+
+export default App;
